9b2661356873d8fd16e68fabb8870b8cebafb09d
tristanbay <85427980+tristanbay@users.noreply.github.com>
2026-04-26 22:17:57 -0700

    Start work on Modem assignment

---
+++ modem/README.md
@@ -0,0 +1,2 @@
+# Modem (CS 416P Assignment)
+This is my program for the "Modem" assignment in Portland State University's CS 416P (Computers, Sound, and Music) course.
---
+++ modem/modem.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2026 me (Tristan); see license.txt for license info
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sndfile.h>
+
+int main()
+{
+
+	return EXIT_SUCCESS;
+}
