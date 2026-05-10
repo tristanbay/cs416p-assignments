a73ad0592b6c54eba53156d60abba3dfc4c58501
tristanbay <85427980+tristanbay@users.noreply.github.com>
2026-05-09 21:01:11 -0700

    Add playback functionality and update readme to match

--- clipped/README.md
+++ clipped/README.md
@@ -2,9 +2,9 @@
 This is my program for the "Clipped" assignment in the CS 416P course (Computers, Sound, and Music) at Portland State University.
 
 ## Setup
-Install the `libsndfile` development package for your Linux distro. The name and command will vary depending on your package maintainer.
-- For Fedora: `sudo dnf install libsndfile-devel`
-- For Ubuntu and Debian: `sudo apt install libsndfile-dev`
+Install the `libsndfile` and `portaudio` development packages for your Linux distro. The name and command will vary depending on your package maintainer.
+- For Fedora: `sudo dnf install libsndfile-devel portaudio-devel`
+- For Ubuntu and Debian: `sudo apt install libsndfile-dev portaudio-dev`
 
 ## Compilation
 Simply run `make` on the command line in the project directory.
@@ -14,6 +14,17 @@ Run the command `./clipped <sine or clipped>` in the project directory.
 - The parameter is the mode choice (sine if no parameter given):
     - `sine` makes the program output a WAV file named `sine.wav` which contains a 1-second sine wave tone at 440 hz and at 1/4 of maximum amplitude.
     - `clipped` makes the program output a WAV file named `clipped.wav` which contains the same as above except twice the previous amplitude but hard-clipped to still be the same 1/4 of maximum amplitude.
+Both options play the generated audio.
+
+## Troubleshooting
+If the audio stutters/is intermittent, try changing the `BUFSIZE` macro on line 17 to a larger value.
+
+## Libraries Used
+libsndfile ([under the LGPL version 2.1 or 3](https://www.gnu.org/licenses/lgpl-3.0.html))
+portaudio ([under the MIT Expat license](https://files.portaudio.com/docs/v19-doxydocs/License.html))
+
+## References Used
+[this source code/web page](https://files.portaudio.com/docs/v19-doxydocs/paex__record_8c_source.html)
 
 ## Licensing
 Copyright © 2026 me (Tristan)
--- clipped/clipped.c
+++ clipped/clipped.c
@@ -1,4 +1,4 @@
-// Assignment for CS 416P at Portland State University, April 2026
+// Assignment for CS 416P at Portland State University, April/May 2026
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,16 +7,55 @@
 #include <stdint.h>
 #include <math.h>
 #include <sndfile.h>
+#include <portaudio.h>
 
 #define PI 3.14159265358979323846
 #define RATE 48000
 #define SECONDS 1
 #define CHANNELS 1
 #define FREQ 440
+#define OUTBUF_SIZE 2048
 #define MODENAME_MAX 8
 #define USAGE_MSG "Usage: \"./clipped <mode (sine or clipped)>\"\n"
 
-int main(int32_t argc, char** argv)
+typedef struct wave_data_s
+{
+	uint32_t current, amt;
+	int16_t* data;
+} wave_data_t;
+
+int32_t play_callback(const void* in_buf, void* out_buf, uint64_t buf_len,
+		const PaStreamCallbackTimeInfo* time_info, PaStreamCallbackFlags flags,
+		void* user_data)
+{
+	wave_data_t* w_data = (wave_data_t*)user_data;
+	int32_t finished, extra;
+	int16_t* read = &(w_data->data[w_data->current]);
+	int16_t* write = (int16_t*)out_buf;
+	int32_t remaining = w_data->amt - w_data->current;
+
+	(void)in_buf;
+	(void)time_info;
+	(void)flags;
+
+	if (remaining < buf_len) {
+		extra = buf_len - remaining;
+		for (int32_t i = 0; i < remaining; ++i)
+			*(write++) = *(read++);
+		for (int32_t i = 0; i < extra; ++i)
+			*(write++) = 0;
+		w_data->current += remaining;
+		finished = paComplete;
+	} else {
+		for (int32_t i = 0; i < buf_len; ++i)
+			*(write++) = *(read++);
+		w_data->current += buf_len;
+		finished = paContinue;
+	}
+	return finished;
+}
+
+int32_t main(int32_t argc, char** argv)
 {
 	uint32_t data_amt = RATE * SECONDS;
 	int16_t wave_data[data_amt];
@@ -66,5 +105,62 @@ int main(int32_t argc, char** argv)
 	sf_write_short(wave, wave_data, data_amt);
 	
 	sf_close(wave);
+
+	PaStreamParameters out_params;
+	PaStream* stream;
+	PaError err = Pa_Initialize();
+	if (err != paNoError) {
+		printf("PortAudio error\n");
+		Pa_Terminate();
+		return EXIT_FAILURE;
+	}
+
+	wave_data_t play_this = { 0, data_amt, &(wave_data[0]) };
+
+	out_params.device = Pa_GetDefaultOutputDevice();
+	if (out_params.device == paNoDevice) {
+		printf("No output device detected\n");
+		Pa_Terminate();
+		return EXIT_FAILURE;
+	}
+	out_params.channelCount = CHANNELS;
+	out_params.sampleFormat = paInt16;
+	out_params.suggestedLatency =
+		Pa_GetDeviceInfo(out_params.device)->defaultLowOutputLatency;
+	out_params.hostApiSpecificStreamInfo = NULL;
+
+	err = Pa_OpenStream(&stream, NULL, &out_params, RATE, OUTBUF_SIZE, paClipOff, play_callback,
+			&play_this);
+	if (err != paNoError) {
+		printf("Error opening stream\n");
+		Pa_Terminate();
+		return EXIT_FAILURE;
+	}
+
+	if (stream) {
+		err = Pa_StartStream(stream);
+		if (err != paNoError) {
+			printf("Error starting stream\n");
+			Pa_Terminate();
+			return EXIT_FAILURE;
+		}
+
+		while ((err = Pa_IsStreamActive(stream)) == 1)
+			Pa_Sleep(125);
+		if (err < 0) {
+			printf("Error playing stream\n");
+			Pa_Terminate();
+			return EXIT_FAILURE;
+		}
+
+		err = Pa_CloseStream(stream);
+		if (err != paNoError) {
+			printf("Error closing stream\n");
+			Pa_Terminate();
+			return EXIT_FAILURE;
+		}
+	}
+
+	Pa_Terminate();
 	return EXIT_SUCCESS;
 }
